343db6d61c2f8bf43a7cbb0e7df1ed00e55f0ade
wassily1 <54395527+wassily1@users.noreply.github.com>
2020-10-20 17:31:38 +0800

    Update lumberjack.go

--- lumberjack.go
+++ lumberjack.go
@@ -118,7 +118,10 @@ type Logger struct {
 var (
 	// currentTime exists so it can be mocked out by tests.
 	currentTime = time.Now
-
+	
+	currentTime1=time.Now()
+	next = currentTime1.Add(time.Hour * 24)
+	nextbreak = time.Date(next.Year(), next.Month(), next.Day(), 0, 0, 0, 0, next.Location())
 	// os_Stat exists so it can be mocked out by tests.
 	os_Stat = os.Stat
 
@@ -136,9 +139,6 @@ func (l *Logger) Write(p []byte) (n int, err error) {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 
-	println("rotate rotate")
-	print("rotate rotate")
-	
 	writeLen := int64(len(p))
 	if writeLen > l.max() {
 		return 0, fmt.Errorf(
@@ -153,25 +153,21 @@ func (l *Logger) Write(p []byte) (n int, err error) {
 	}
 
 	if l.size+writeLen > l.max() {
-		println("rotate max")
 		if err := l.rotate(); err != nil {
 			return 0, err
 		}
 	}
 
-	//wassily添加的：
-	now := time.Now()  //获取当前时间，放到now里面，要给next用  
-        next := now.Add(time.Hour * 24) //通过now偏移24小时
-        next = time.Date(next.Year(), next.Month(), next.Day(), 0, 0, 0, 0, next.Location()) //获取下一个凌晨的日期
-        t := time.NewTimer(next.Sub(now))//计算当前时间到凌晨的时间间隔，设置一个定时器
-        <-t.C
-	{
-		println("rotate time")
+	now:=time.Now()
+	
+	if now.After(nextbreak){
 		if err := l.rotate(); err != nil {
 			return 0, err
 		}
+		currentTime1=time.Now()
+		next = currentTime1.Add(time.Hour * 24)
+		nextbreak = time.Date(next.Year(), next.Month(), next.Day(), 0, 0, 0, 0, next.Location())
 	}
-	
 	
 	n, err = l.file.Write(p)
 	l.size += int64(n)
