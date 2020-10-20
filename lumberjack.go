aed603051457e717b9ac53b3cd98d0bb34634934
wassily1 <54395527+wassily1@users.noreply.github.com>
2020-10-20 13:55:21 +0800

    Update lumberjack.go

--- lumberjack.go
+++ lumberjack.go
@@ -155,6 +155,16 @@ func (l *Logger) Write(p []byte) (n int, err error) {
 		}
 	}
 
+	//wassily添加的：
+	now := time.Now()  //获取当前时间，放到now里面，要给next用  
+        next := now.Add(time.Hour * 24) //通过now偏移24小时
+        next = time.Date(next.Year(), next.Month(), next.Day(), 0, 0, 0, 0, next.Location()) //获取下一个凌晨的日期
+        t := time.NewTimer(next.Sub(now))//计算当前时间到凌晨的时间间隔，设置一个定时器
+        <-t.C
+	if err := l.rotate(); err != nil {
+			return 0, err
+		}
+	
 	n, err = l.file.Write(p)
 	l.size += int64(n)
 
